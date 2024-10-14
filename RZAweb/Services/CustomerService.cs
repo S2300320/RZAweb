6b29a01ae49fc2eaf403449d25a561e0ae24fd8a
Alpha Bah <S2300320@ndonline.ac.uk>
2024-10-14 12:49:46 +0100

    Add project files.

---
+++ RZAweb/Services/CustomerService.cs
@@ -0,0 +1,15 @@
+﻿using RZAweb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RZAweb.Services
+{
+    public class CustomerService
+    {
+        private readonly TlS2300320RzaContext _context;
+
+        public CustomerService(TlS2300320RzaContext context)
+        {
+            _context = context;
+        }
+    }
+}
